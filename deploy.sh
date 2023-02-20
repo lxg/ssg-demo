48e7a5a4baf9092bc66897946b75b387bd42e2f5
Alexander Günsche <lxg@amazon.com>
2023-02-20 16:36:22 +0100

    simple SSG demo

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-central-1}
+export STACK_NAME=${STACK_NAME:-ssg-demo}
+
+dir=$(pwd)
+
+cd $dir
+[ -d dist ] && rm -r dist
+[ -d node_modules ] || npm ci --omit=dev --no-audit
+npm run build
+
+cd $dir
+aws cloudformation deploy \
+    --stack-name=$STACK_NAME \
+    --template-file=cloudformation.yml \
+    --capabilities CAPABILITY_IAM \
+    --no-fail-on-empty-changeset
+
+bucketName=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`bucketName`].OutputValue' --output text)
+cftDist=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`cftDist`].OutputValue' --output text)
+cftDomain=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`cftDomain`].OutputValue' --output text)
+
+aws s3 cp $dir/dist/ s3://$bucketName/ --recursive --cache-control max-age=60
+aws cloudfront create-invalidation --distribution-id $cftDist --paths "/*"
+
+echo "Done, deployed at https://$cftDomain"
